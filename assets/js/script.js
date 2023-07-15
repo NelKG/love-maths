450503f1019c4c719c23d1d92f8b802a682e34cc
NelKG <>
2023-07-15 13:44:08 +0000

    add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,31 @@
 // get the button elements and add event listeners to them
 // /wait for the DOM to finish load before running the game
 
-document.addEventListener("DOMContentLoaded", function(){
-    let buttons =documentgetElementByTagName("button");
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {     
-            if(this.getAttribute("data-type") === "submit") {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
             }
-            })
-        }
-        })
-       
+        });
+    }
+});
 
-    
-
-
-function runGame(){
+function runGame() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 function checkAnswer() {
 
 }
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
 
 }
@@ -40,11 +39,12 @@ function incrementWrongAnswer() {
 function displayAdditionalQuestion() {
 
 }
-function displaySubtractQuestion(){
+function displaySubtractQuestion() {
 
 
 }
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion() {
 
 
-}
+
+
